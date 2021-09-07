85c21ca8c448121a33f82f449547b14c6376887e
Luisil41 <luisisarrial@gmail.com>
2021-09-07 15:27:50 +0200

    FEAT: DB connected

---
+++ utils/mongodb.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const mongodb = process.env.MONGODB_URL;
+
+const connect = async () => {
+  try {
+    const db = await mongoose.connect(MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const { name, host } = db.connection;
+    console.log(`Conectado correctamente a la DB ${name}`);
+  } catch (error) {
+    console.log("No se pudo conectar a la DB");
+  }
+};
