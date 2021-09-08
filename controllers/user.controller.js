5e4216363127b0ff77095046a3999a63fbc1437f
Luisil41 <luisisarrial@gmail.com>
2021-09-08 11:52:59 +0200

    FEAT: user controller & users reviewed

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -10,3 +10,44 @@ const getById = async (req, res, next) => {
     return next(error);
   }
 };
+
+const editById = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id);
+    return res.json(user);
+
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const putById = async (req, res, next) => {
+  const { id } = req.params;
+  const { fullName,birthdate, email, password, avatar, phone, province, interest } = req.body
+
+  try {
+
+    const update = {};
+    if(fullName) update.fullName = fullName;
+    if(birthdate) update.birthdate = birthdate;
+    if(email) update.email = email;
+    //if(password) update.password = password;
+    //if(avatar) update.avatar = avatar;
+    if(phone) update.phone = phone;
+    if(province) update.province = province;
+    if(interest) update.interest = interest;
+
+
+    const updateUser = await User.findByIdAndUpdate(id,update, { new: true });
+    return res.redirect(`/user/${updateUser._id}`);
+
+  } catch (error) {
+    return next(error)
+  }
+}
+
+
+
+module.exports = { getById, editById, putById };
--- routes/User.routes.js
+++ routes/User.routes.js
@@ -5,3 +5,9 @@ const controller = require('../controllers/user.controller');
 const router = express.Router();
 
 
+
+router.get('/edit', controller.editById);
+
+router.put('/edit', controller.putById);
+
+router.get('/:id', controller.getById);
