fb55261a3baf941fab0c84f5b1789e465bb219a2
Guille <dende8bruselas@gmail.com>
2021-09-08 18:56:41 +0200

    FEAT: login and register strategy

--- auth/utils.js
+++ auth/utils.js
@@ -1,7 +1,3 @@
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('crypt');
-const User = require('../models/User.model');
-
 const validateEmail = (email) => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
@@ -11,3 +7,8 @@ const validatePass = (password) => {
     const re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
     return re.test(String(password));
 };
+
+module.exports = { 
+    validateEmail,
+    validatePass
+}
