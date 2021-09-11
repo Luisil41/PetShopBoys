3414dc6740b9f20e5c693b3fb9718bc972e4df7f
Rut Elisenda Diaz Albert <diaz.albert@bootcamp-upgrade.com>
2021-09-11 17:06:22 +0200

    Request controller

--- models/Request.model.js
+++ models/Request.model.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const requestSchema = new Schema(
-    {
-        userId: { type: mongoose.Types.ObjectId, ref: 'User'},
-        shelterId: { type: mongoose.Types.ObjectId, ref: 'Shelter'},
-        petId: { type: mongoose.Types.ObjectId, ref: 'Pet'},
-    },
-    { timestamps: true }
-)
+const requestSchema = new Schema({
+    userId: { type: mongoose.Types.ObjectId, ref: 'User' },
+    shelterId: { type: mongoose.Types.ObjectId, ref: 'Shelter' },
+    petId: { type: mongoose.Types.ObjectId, ref: 'Pet' },
+    message: { type: String }
+}, { timestamps: true })
 
 const Request = mongoose.model('Request', requestSchema);
 
